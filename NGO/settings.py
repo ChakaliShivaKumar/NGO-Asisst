b0b159d9060fc726d61203420daa9d744383a40c
ChakaliShivaKumar <84917394+ChakaliShivaKumar@users.noreply.github.com>
2022-07-24 16:44:34 +0530

    Update settings.py

--- NGO/settings.py
+++ NGO/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-27$%uj@v!%9&@ld+dy6^#irf9ig1^=hotybg!57@!2o4=be_ai'
+# SECRET_KEY = 'django-insecure-27$%uj@v!%9&@ld+dy6^#irf9ig1^=hotybg!57@!2o4=be_ai'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
